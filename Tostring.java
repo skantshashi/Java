4b499e2d8d35562984d233dcf40d89b4d251350c
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-01 02:19:14 +0530

    tostring method

---
+++ Tostring.java
@@ -0,0 +1,10 @@
+public class Tostring {
+    public static void main(String args[]) {
+        int number = 123;
+        String str = Integer.toString(number);
+        System.out.println(str.length());
+        
+        
+     }
+  
+}
