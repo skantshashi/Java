43daf0f9defa74780eff493ceafcea89dc95bb40
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-01 02:14:43 +0530

    Substring

---
+++ substring.java
@@ -0,0 +1,7 @@
+public class substring {
+    public static void main(String[] args) {
+        String name = "TonyStark";
+              
+        System.out.println(name.substring(0, 4));
+    }
+}
