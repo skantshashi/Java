1a9f08f85410d123dad6811fd1e8919d8d48753b
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-01 17:19:52 +0530

    clear the  bit at i th position of a number

---
+++ clearBit.java
@@ -0,0 +1,10 @@
+public class clearBit {
+    public static void main(String args[]) {
+        int n = 5; //0101
+        int pos = 2;
+        int bitMask = 1<<pos;
+        int newBitMask = ~(bitMask);
+        int newNumber = newBitMask & n;
+        System.out.println(newNumber);
+     }
+}
