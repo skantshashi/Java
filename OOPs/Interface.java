a8b77b453b2061d0396846f81c08bee9f92afde8
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-16 03:37:20 +0530

    update

---
+++ OOPs/Interface.java
@@ -0,0 +1,23 @@
+interface Animal1 {
+    void walk();
+ }
+  
+ class Horse1 implements Animal1 {
+    public void walk() {
+        System.out.println("Horse walks on 4 legs");
+    }
+ }
+  
+ class Chicken1 implements Animal1 {
+    public void walk() {
+        System.out.println("Chicken walks on 2 legs");
+    }
+ }
+  
+ public class Interface{
+    public static void main(String args[]) {
+       Horse1 horse = new Horse1();
+       horse.walk();
+    }
+ }
+ 
