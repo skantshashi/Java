24c18f52caecf26e111bc8ac52a2fcca7cbe723c
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-15 19:37:55 +0530

    update

--- OOPs/FunctionOverloading.java
+++ OOPs/FunctionOverloading.java
@@ -1,7 +1,13 @@
 class Student{
     String name;
     int age;
-    public void info(){
+    public void info(String name){
+        System.out.println(this.name);
+    }
+    public void info(int age){
+        System.out.println(this.age);
+    }
+    public void info(String name, int age){
         System.out.println(this.name+" "+this.age);
     }
     Student(){//default constructor
@@ -14,5 +20,12 @@ class Student{
     }
 }
 public class FunctionOverloading {
-    
+    public static void main(String[] args) {
+        Student s1=new Student();
+         s1.name="hare krishna";
+         s1.age=23;
+         s1.info(s1.name);
+         s1.info(s1.age);
+         s1.info(s1.name, s1.age);
+        }
 }
