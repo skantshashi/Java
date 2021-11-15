03c0a66e1295ec51d89e1bb8632126f8e026d0c8
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-15 18:52:33 +0530

    update

---
+++ OOPs/CopyConstructor.java
@@ -0,0 +1,24 @@
+class Student{
+    String name;
+    int age;
+    public void info(){
+        System.out.println(this.name+" "+this.age);
+    }
+    Student(){
+
+    }
+       
+    Student(Student s2){
+       this.name=s2.name;
+       this.age=s2.age;
+    }
+}
+public class CopyConstructor {
+    public static void main(String[] args) {
+    Student s1=new Student();
+     s1.name="hare krishna";
+     s1.age=23;
+     Student s2=new Student(s1);
+     s2.info();
+    }
+}
