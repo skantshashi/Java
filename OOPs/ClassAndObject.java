f028bf4cc710c46b213f025b3e7c3a94f15ea4e5
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-15 18:25:21 +0530

    update

--- OOPs/ClassAndObject.java
+++ OOPs/ClassAndObject.java
@@ -4,12 +4,18 @@ class Student{
     public void info(){
         System.out.println(this.name+" "+this.age);
     }
+    Student(){
+        System.out.println("constructor is called ");
+    }
+    Student(String name, int age){
+
+    }
 }
 public class ClassAndObject {
     public static void main(String[] args) {
         Student s1=new Student();
-        s1.name="shashi";
-        s1.age=23;
+        //s1.name="shashi";
+        //s1.age=23;
         s1.info();
     }
 }
