dcd963a233470cd89848309a28e052cb97ea397a
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-17 00:43:29 +0530

    update

---
+++ OOPs/Recursion/Recursion3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Recursion3 {
+    public static int pow(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        if(n%2==0){
+            return pow(x,n/2)*pow(x, n/2);
+        }
+        else{
+            return pow(x,n/2)*pow(x, n/2)*x;
+        }
+    }
+    public static void main(String[] args) {
+        int x=2,n=5;
+        System.out.println(pow(x,n));
+    }
+}
