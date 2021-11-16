4a05dab3056ee834c9243a0e0b79e251e34fb0d4
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-17 00:23:10 +0530

    update

--- OOPs/Recursion/Recursion2.java
+++ OOPs/Recursion/Recursion2.java
@@ -4,6 +4,9 @@ import java.util.*;
         if(n==0){
             return 1;
         }
+        if(x==0){
+            return 0;
+        }
         return x*power(x, n-1);
     }
     public static void main(String[] args) {
