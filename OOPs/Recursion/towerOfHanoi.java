06c9f5f7c9d91aa581c98c7b21ea455f85ccf853
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-17 01:56:40 +0530

    update

---
+++ OOPs/Recursion/towerOfHanoi.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class towerOfHanoi {
+    public static void towerOfHanoi(int n, String src, String helper, String dest){
+        if(n==1){
+            System.out.println("Transfer Disk "+n+" from "+src+" to "+dest);
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, helper);
+        System.out.println("Transfer Disk "+n+" from "+src+" to "+dest);
+        towerOfHanoi(n-1, helper, helper, dest);
+    }
+    public static void main(String[] args) {
+        int n=2;
+        towerOfHanoi(n, "S", "H", "D");
+    }
+}
