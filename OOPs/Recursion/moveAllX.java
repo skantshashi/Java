c9d04e198192b69158b5b1d46e60d5cc07650d74
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-17 04:22:20 +0530

    update

--- OOPs/Recursion/moveAllX.java
+++ OOPs/Recursion/moveAllX.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
 public class moveAllX {
-    public static void moveAllX(String str, int idx,int count,String newString){
+    public static void moveAllX(String str, int idx,int count, char element,String newString){
         if(idx==str.length()){
             for(int i=0;i<count;i++){
-                newString+='x';
+                newString+=element;
             }
             System.out.print(newString);
             return;
         }
         char current=str.charAt(idx);
-        if(current=='x'){
+        if(current== element){
             count++;
-            moveAllX(str, idx+1, count, newString);
+            moveAllX(str, idx+1, count,element, newString);
         }
         else{
             newString+=current;
-            moveAllX(str, idx+1, count,newString);
+            moveAllX(str, idx+1, count, element, newString);
         }
     }
     public static void main(String[] args) {
         String str="axbcxxd";
-        moveAllX(str,0,0,"");
+        moveAllX(str,0,0,'x',"");
     }
 }
