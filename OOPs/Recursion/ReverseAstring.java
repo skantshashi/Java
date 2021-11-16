04d3c7618ea8a8d323e8b96e97f4369c7a37a3dc
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-17 03:42:33 +0530

    update

--- OOPs/Recursion/ReverseAstring.java
+++ OOPs/Recursion/ReverseAstring.java
@@ -1,13 +1,13 @@
 import java.util.*;
 // reverse string using recursion 
 public class ReverseAstring {
-    public static void revStr(String str, int indx){
-        if(indx==0){
-            System.out.print(str.charAt(indx));
+    public static void revStr(String str, int idx){
+        if(idx==0){
+            System.out.print(str.charAt(idx));
             return;
         }
-        System.out.print(str.charAt(indx));
-        revStr(str, indx-1);
+        System.out.print(str.charAt(idx));
+        revStr(str, idx-1);
     }
     public static void main(String[] args) {
         String str="abcd jfas jdlka";
