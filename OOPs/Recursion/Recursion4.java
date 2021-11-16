378a3e9a3ae7b956ff4dbf4d587fda4318374a84
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-17 03:35:48 +0530

    update

--- OOPs/Recursion/Recursion4.java
+++ OOPs/Recursion/Recursion4.java
@@ -5,8 +5,8 @@ public class Recursion4 {
     public static int last=-1;
     public static void findOcurrence(String str, int idx, char element){
         if(idx==str.length()){
-            System.out.println(first);
-            System.out.println(last);
+            System.out.print("first Occurrence of index  at : "+first+"\n");
+            System.out.println("last Occurrence of index  at : "+last);
             return;
         }
         char current= str.charAt(idx);
