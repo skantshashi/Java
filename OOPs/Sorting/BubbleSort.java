e787fc46871ea90ec5aa5343efb25bdd50b7daf6
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-20 03:59:43 +0530

    update

--- OOPs/Sorting/BubbleSort.java
+++ OOPs/Sorting/BubbleSort.java
@@ -1,9 +1,9 @@
 public class BubbleSort {
     public static void main(String[] args) {
         int A[]={7,8,1,3,2,8,7,9,0,2};
-        //bubble sort
+        //bubble sort 
         for(int i=0;i<A.length-1;i++){
-            for(int j=0;j<A.length-1-i;j++){
+            for(int j=0;j<A.length-1-i;j++){//-i because last index was sorted
               if(A[j]>A[j+1]){
                   int temp;
                   temp=A[j];
