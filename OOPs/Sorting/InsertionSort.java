f1db512ba09fd4fd5f5dce0a09bf5774752142bb
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-20 04:38:37 +0530

    update

--- OOPs/Sorting/InsertionSort.java
+++ OOPs/Sorting/InsertionSort.java
@@ -2,7 +2,7 @@ public class InsertionSort {
     public static void main(String[] args) {
         int a[]={9,8,6,4,5,2};
         for(int i=1;i<a.length;i++){
-            int j=i-1;
+            int j=i-1;//sorted array index
             int current=a[i];
             while(j>=0 && current<a[j]){
                 a[j+1]=a[j];
