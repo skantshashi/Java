17a2b2b151974f0ba01f2e47d24889dd564944da
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-20 04:37:22 +0530

    update

--- OOPs/Sorting/InsertionSort.java
+++ OOPs/Sorting/InsertionSort.java
@@ -4,11 +4,11 @@ public class InsertionSort {
         for(int i=1;i<a.length;i++){
             int j=i-1;
             int current=a[i];
-            while(j>=0 && current>a[j]){
-                current=a[j];
+            while(j>=0 && current<a[j]){
+                a[j+1]=a[j];
                 j--;
             }
-            
+            a[j+1]=current;
         }
         for(int i=0;i<a.length;i++){
             System.out.print(a[i]+" ");
