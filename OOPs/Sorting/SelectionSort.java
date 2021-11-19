cd752c96a30d5b0f5d15486741711d4a101defa8
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-20 03:58:30 +0530

    update

--- OOPs/Sorting/SelectionSort.java
+++ OOPs/Sorting/SelectionSort.java
@@ -2,7 +2,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         int a[]={9,8,0,5,6,3,2};
         // Selection sort means one itteration one swap
-        for(int i=0;i<a.length-1;i++){
+        for(int i=0;i<a.length-1;i++){//length minus one because last element will automatically sorted
             int smallest=i;
             for(int j=i+1;j<a.length;j++){
                 if(a[smallest]>a[j]){
