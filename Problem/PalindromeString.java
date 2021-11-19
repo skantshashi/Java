5eca61561f8d9b58e7111ec058f60569bb2c41d5
skantshashi <74304302+skantshashi@users.noreply.github.com>
2021-11-19 23:59:54 +0530

    update

---
+++ Problem/PalindromeString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sk=new Scanner(System.in);
+        System.out.print("enter the string :");
+        String str=sk.nextLine();
+        String strr="";
+        int i=0;
+        System.out.println(str.charAt(i));
+    }
+}
